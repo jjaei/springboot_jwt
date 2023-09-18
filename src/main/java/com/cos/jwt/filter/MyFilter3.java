ef2f041703f248050f86b330379a306e7946a63f
jjaei <ieajnowk@gmail.com>
2023-09-19 03:09:10 +0900

    jwt 임시 토큰 테스트

--- src/main/java/com/cos/jwt/filter/MyFilter3.java
+++ src/main/java/com/cos/jwt/filter/MyFilter3.java
@@ -5,14 +5,34 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyFilter3 implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("필터3");
-        chain.doFilter(request, response);
 
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.setCharacterEncoding("UTF-8");
+
+        // 토큰을 만들었다고 가정(cos)
+        if (req.getMethod().equals("POST")) {
+            System.out.println("POST 요청");
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println("headerAuth = " + headerAuth);
+
+            if (headerAuth.equals("cos")) {
+                chain.doFilter(req, res);
+            } else {
+                PrintWriter out = res.getWriter();
+                out.println("인증이 되지 않았습니다.");
+            }
+        }
+
+        System.out.println("필터3");
     }
 }
